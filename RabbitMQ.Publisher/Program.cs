74ee03e95816117afdd2b352887dc35dabadd871
sinanarslan <sinan.arslan@meb.gov.tr>
2024-12-12 12:46:04 +0300

    Add project files.

---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using RabbitMQ.Client;
+using System.Data;
+using System.Text;
+using System.Threading.Channels;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+
+
+        //!!! excahge oluşturmayı da publisher yaparken bazı durumlarda  Kuyruk oluşturmayı  consumer da yapabilir. Ancak bu durum istisna ve dikkatli olmak lazım.
+        // Fanout Exchange kendisine bağlı olan tüm kuyruklara aynı mesajı iletir.
+        // Kaç tane consumer bağlanacağını bilmediğimiz durumlarda burada kullanıcılara kendi kuyruklarınızı oluşturup hava durumlarını çekebilirsiniz. Biz saate bir Fanout exchange ile bir kanal oluşturuyoruz buradan isteyen alsın diyebilirsin.
+        // Kullanmak istemeyen kuyruğunu kapatsın gitsin.
+        
+        // Fanout da genelde consumer lar kendi kuyruklarını gönderiyor. Önceki gönderdiklerimin önemi olmadığından kuyruğunu oluşturup bağlanan gelip yeni tahminleri almaya başlasın.
+
+        // 
+
+
+
+
+
+
+
+
+        IConnection con = factory.CreateConnectionAsync().Result;// connection oluştur
+        IChannel channel = con.CreateChannelAsync().Result;   // rabbitmq ya bir kanal oluştur
+
+        channel.QueueDeclareAsync("RabbitMQSinan", true, false, false,null);
+
+
+        Enumerable.Range(1, 50).ToList().ForEach(x =>
+        {
+            string mesaj = $"Merhaba Rabbit Ben Geldim {x} ";
+            byte[] mesajbody = Encoding.UTF8.GetBytes(mesaj); // RabitMQ byte[] dizisi alır.
+            channel.BasicPublishAsync(string.Empty, "RabbitMQSinan", mesajbody);
+
+            Console.WriteLine("Mesaj Gönderildi : "+ mesaj);
+        });
+
+
+     
+     
+
+
+
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMQComsumer/Program.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+
+
+        await Hadi();
+
+        Console.ReadLine();
+    }
+    public static async Task Hadi()
+    {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.HostName = "localhost";
+        factory.Port = 5672;
+        factory.UserName = "guest";
+        factory.Password = "guest";
+
+        using IConnection con = await factory.CreateConnectionAsync();// connection oluştur
+        using IChannel channel = await con.CreateChannelAsync();   // rabbitmq ya bir kanal oluştur
+
+       await channel.BasicQosAsync(0, 1, false); // Her bir subscribe 5 er 5 er gönder  //  true yaparsak tüm subscriberlar için 5 adet olacak şekilde böl demiş oluyoruz.
+        
+
+
+        //    channel.QueueDeclareAsync("RabbitMQSinan", true, false, false); // silinebilir de. Eminsek kuyruk olduğunda
+        var consumer = new AsyncEventingBasicConsumer(channel);
+        consumer.ReceivedAsync += Consumer_ReceivedAsync;
+        await channel.BasicConsumeAsync("RabbitMQSinan", autoAck: false, consumer: consumer); //autoAck: Bunu false yaparsak işlem bitince biz sil diyebileceğiz
+        Console.ReadLine();
+
+    }
+
+    private static Task Consumer_ReceivedAsync(object sender, BasicDeliverEventArgs @event)
+    {
+        var body = @event.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+
+        Thread.Sleep(2000);
+
+
+        Console.WriteLine($" [x] Received {message}");
+        ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(@event.DeliveryTag, false); // Hangi tagla buraya gelmişse. O taglı kuyruğu sil, false ise Diğerlerini silsin mi, hayır
+
+
+
+
+        return Task.CompletedTask;
+    }
+}
